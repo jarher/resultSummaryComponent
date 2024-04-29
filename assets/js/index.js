5c40a105bd18f9ec8776c34e09f74be9dfd0c9f8
jarher <jefferh33@gmail.com>
2024-04-29 16:44:51 -0500

    desktop css styles finished

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+async function getData() {
+  const res = await fetch("../../data.json");
+  return res.json();
+}
+function asignClass(category) {
+  if (category === "Reaction") return "reaction-dt";
+  if (category === "Memory") return "memory-dt";
+  if (category === "Verbal") return "verbal-dt";
+  if (category === "Visual") return "visual-dt";
+}
+
+function template({ category, score, icon }) {
+  return `<dt class="${asignClass(category)}">${category}</dt>
+        <dd><b>${score}</b> / 100</dd>`;
+}
+async function insertInDOM() {
+  const res = await getData();
+  const categories = res.reduce(
+    (acc, current) => (acc += template(current)),
+    ""
+  );
+  document.querySelector("dl").innerHTML = categories;
+}
+
+insertInDOM();
